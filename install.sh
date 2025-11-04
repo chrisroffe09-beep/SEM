4bee2cc5a15cc9cd39fcb9a1c8d46b0527a2940d
chrisroffe09-beep <chris.roffe09@gmail.com>
2025-11-04 09:14:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@
 # Make launcher executable and copy the ssm pkg to /usr/local/bin
 chmod +x ssm
 
-sudo cp ssm_pkg/main.py /usr/local/bin/ssm_pkg/main.py
+sudo mkdir /usr/local/bin/ssm_pkg
+sudo touch /usr/local/bin/ssm_pkg/main.py
+sudo cp ./ssm_pkg/main.py /usr/local/bin/ssm_pkg/main.py
 # Copy launcher to /usr/local/bin
 echo "Installing SSM globally..."
 if sudo cp ssm /usr/local/bin/ssm; then
