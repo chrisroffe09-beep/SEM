ba9b0701c3891f664c568407d223bca227af510f
SourMitten170 <chris.roffe09@gmail.com>
2025-11-05 23:15:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ sudo cp ./ssm_pkg/main.py /usr/local/bin/ssm_pkg/main.py
 # Copy launcher to /usr/local/bin
 echo "Installing SSM globally..."
 if sudo cp ssm /usr/local/bin/ssm; then
-    echo "SSM installed globally! You can now run 'ssm' from any terminal."
+    echo "SSM installed globally! You can now run 'sudo ssm' from any terminal."
 else
     echo "Failed to copy launcher to /usr/local/bin. Try running: sudo bash install.sh"
 fi
